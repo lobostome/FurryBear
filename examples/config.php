2765fef48671487aead0dccbb4dc4d63bd5c7683
lobostome <lobostome@local.dev>
2013-03-10 15:02:02 -0500

    Refactor example Sunlight Foundation API key use

--- examples/config.php
+++ examples/config.php
@@ -6,7 +6,7 @@ $classLoader = new SplClassLoader(__DIR__ . '/../src');
 $classLoader->register();
 
 // Substitute $apiKey with your API key.
-$apikeyFile = 'apikey.local';
+$apikeyFile = 'apikey-sf.local';
 $apiKey = (file_exists($apikeyFile)) 
                     ? file_get_contents($apikeyFile) 
                     : 'xxxxxxxxxxxxxxxxxxxxxxxxxxx';
