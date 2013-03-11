9304c20be347c49b8123eedf948868e1bdb99edd
lobostome <lobostome@local.dev>
2013-03-11 01:52:06 -0500

    Add Bings Maps geocode provider

--- examples/helpers.php
+++ examples/helpers.php
@@ -8,11 +8,19 @@ $address = '2130 Fulton Street, San Francisco, CA 94117';
 try {
     var_dump($fb->legislators_locate->getByZip($zip));
     
-    $geocodeProvider = new \FurryBear\Geocode\Provider\GoogleMaps();
+    $googleMaps = new \FurryBear\Geocode\Provider\GoogleMaps();
     
-    var_dump($fb->legislators_locate->via($geocodeProvider)
+    var_dump($fb->legislators_locate->via($googleMaps)
                                     ->getByAddress($address));
     
+    $bingMapsApiKey = (file_exists(__DIR__ . '/config/keys/apikey-bingmaps.local')) 
+                            ? file_get_contents(__DIR__ . '/config/keys/apikey-bingmaps.local') 
+                            : 'xxxxxxxxxxxxxxxxxxxxxxxxxxx';
+    
+    $bingMaps = new \FurryBear\Geocode\Provider\BingMaps($bingMapsApiKey);
+    
+    var_dump($fb->legislators_locate->via($bingMaps)
+                                    ->getByAddress($address));
 } catch (\Exception $e) {
     echo $e->getMessage();
 }
---
+++ src/FurryBear/Geocode/Provider/BingMaps.php
@@ -0,0 +1,94 @@
+<?php
+
+/**
+ * FurryBear
+ * 
+ * PHP Version 5.3
+ * 
+ * @category Congress_API
+ * @package  FurryBear
+ * @author   lobostome <lobostome@local.dev>
+ * @license  http://opensource.org/licenses/MIT MIT License
+ * @link     https://github.com/lobostome/FurryBear
+ */
+
+namespace FurryBear\Geocode\Provider;
+
+use FurryBear\Geocode\AbstractProvider,
+    FurryBear\Exception\InvalidCredentialsException,
+    FurryBear\Exception\NoResultException;
+
+/**
+ * Bing Maps geocode provider
+ * 
+ * @category Congress_API
+ * @package  FurryBear
+ * @author   lobostome <lobostome@local.dev>
+ * @license  http://opensource.org/licenses/MIT MIT License
+ * @link     http://msdn.microsoft.com/en-us/library/ff701714.aspx
+ */
+
+class BingMaps extends AbstractProvider
+{
+    /**
+     * The Geocoding API request URL
+     */
+    const ENDPOINT_URL = 'http://dev.virtualearth.net/REST/v1/Locations?q=%s&key=%s';
+    
+    /**
+     * The Geocoding API request URL over HTTPS
+     */
+    const ENDPOINT_URL_SSL = 'https://dev.virtualearth.net/REST/v1/Locations?q=%s&key=%s';
+
+    /**
+     * Enable use of HTTPS request
+     * 
+     * @var boolean
+     */
+    protected $useSsl;
+    
+    /**
+     * The service API key
+     * 
+     * @var string
+     */
+    protected $apiKey;
+    
+    /**
+     * Construct the geocoder with a API key and instruct whether the request is
+     * made over HTTPS
+     * 
+     * @param string  $apiKey The service API key
+     * @param boolean $useSsl Use HTTPS or not
+     */
+    public function __construct($apiKey, $useSsl = false)
+    {
+        if (empty($apiKey)) {
+            throw new InvalidCredentialsException('Bing Maps requires an API key');
+        }
+        $this->apiKey = $apiKey;
+        $this->useSsl = $useSsl;
+    }
+    
+    /**
+     * {@inheritdoc}
+     * 
+     * @param string $address The target address
+     * 
+     * @return array
+     */
+    public function geocode($address)
+    {
+        $url = sprintf($this->useSsl ? self::ENDPOINT_URL_SSL : self::ENDPOINT_URL, urlencode($address), $this->apiKey);
+        $jsonObj = $this->get($url);
+        
+        if (isset($jsonObj->resourceSets[0]) && isset($jsonObj->resourceSets[0]->resources[0])) {
+            $result = $jsonObj->resourceSets[0]->resources[0];
+        } else {
+            throw new NoResultException('Could not get results from Bing Maps');
+        }
+                
+        return array('latitude'  => $result->geocodePoints[0]->coordinates[0], 
+                     'longitude' => $result->geocodePoints[0]->coordinates[1]);
+    }
+}
