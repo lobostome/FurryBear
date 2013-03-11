c40da02894a3cb99d749eb86c8e64c2b96d4cf0d
lobostome <lobostome@local.dev>
2013-03-11 00:46:25 -0500

    Add Google Maps geocode provider, add Sunlight Foundation getByAddress legislator

--- examples/helpers.php
+++ examples/helpers.php
@@ -3,9 +3,16 @@
 require_once 'config/config_curl.php';
 
 $zip = '94102';
+$address = '2130 Fulton Street, San Francisco, CA 94117';
 
 try {
     var_dump($fb->legislators_locate->getByZip($zip));
+    
+    $geocodeProvider = new \FurryBear\Geocode\Provider\GoogleMaps();
+    
+    var_dump($fb->legislators_locate->via($geocodeProvider)
+                                    ->getByAddress($address));
+    
 } catch (\Exception $e) {
     echo $e->getMessage();
 }
--- src/FurryBear/Resource/SunlightFoundation/LegislatorsLocate.php
+++ src/FurryBear/Resource/SunlightFoundation/LegislatorsLocate.php
@@ -30,6 +30,13 @@ class LegislatorsLocate extends BaseResource
      * string.
      */
     const LEGISLATORS_LOCATE_METHOD = 'legislators/locate';
+    
+    /**
+     * The geocoding service provider
+     * 
+     * @var \FurryBear\Geocode\AbstractProvider 
+     */
+    protected $geocodeProvider;
 
     /**
      * Constructs the resource, sets a reference to the FurryBear object, and 
@@ -54,5 +61,34 @@ class LegislatorsLocate extends BaseResource
     {
         $params = array('zip' => (string)$zip);
         return $this->get($params);
+    }
+    
+    /**
+     * Retrieves legislators based on an address
+     * 
+     * @param string $address The target address
+     * 
+     * @return mixed
+     */
+    public function getByAddress($address)
+    {
+        $params = $this->geocodeProvider->geocode($address);
+        return $this->get($params);
+    }
+    
+    /**
+     * Set the geocode provider and its adapter and output strategy.
+     * 
+     * @param \FurryBear\Geocode\AbstractProvider $provider The geocode provider
+     * 
+     * @return \FurryBear\Resource\SunlightFoundation\LegislatorsLocate
+     */
+    public function via(\FurryBear\Geocode\AbstractProvider $provider)
+    {
+        $this->geocodeProvider = $provider;
+        $this->geocodeProvider->setAdapter($this->furryBear->getProvider()->getAdapter());
+        $this->geocodeProvider->setOutput(new \FurryBear\Output\Strategy\JsonToObject);
+        
+        return $this;
     }
 }
