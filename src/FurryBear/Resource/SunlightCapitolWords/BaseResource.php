1c0ab440cb4814bc907c0816a657a878d65ea0bc
lobostome <lobostome@local.dev>
2013-06-24 11:20:51 -0500

    Add Sunlight Capitol Words provider and base resource

---
+++ src/FurryBear/Resource/SunlightCapitolWords/BaseResource.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * FurryBear
+ * 
+ * PHP Version 5.3
+ * 
+ * @category Congress_API
+ * @package  FurryBear
+ * @author   lobostome <lobostome@local.dev>
+ * @license  http://opensource.org/licenses/MIT MIT License
+ * @link     https://github.com/lobostome/FurryBear
+ */
+
+namespace FurryBear\Resource\SunlightCapitolWords;
+
+use FurryBear\Resource\AbstractResource;
+
+/**
+ * A base presentation of SunlightCapitolWords resource.
+ * 
+ * @category Congress_API
+ * @package  FurryBear
+ * @author   lobostome <lobostome@local.dev>
+ * @license  http://opensource.org/licenses/MIT MIT License
+ * @link     https://github.com/lobostome/FurryBear
+ */
+class BaseResource extends AbstractResource
+{
+    /**
+     * Construct a resource with a FurryBear instance.
+     * 
+     * @param \FurryBear\FurryBear $furryBear A FurryBear instance.
+     */
+    public function __construct(\FurryBear\FurryBear $furryBear)
+    {
+        parent::__construct($furryBear);
+    }
+
+    /**
+     * {@inheritdoc}
+     * 
+     * @param array $params The search criteria.
+     * 
+     * @return string
+     */
+    protected function buildQuery(array $params)
+    {
+        $apiKey = array();
+        if (method_exists($this->furryBear->getProvider(), 'getApiKey')) {
+            $apiKey['apikey'] = $this->furryBear->getProvider()->getApiKey();
+        }
+        
+        // convert booleans to string representation
+        array_walk ($params, function(&$item, $key) {
+                if (is_bool($item)) {
+                    $item = ($item) ? 'true' : 'false';
+                }
+            }
+        );
+        
+        return $this->furryBear->getProvider()->getServiceUrl() 
+               . '/' .
+               $this->getResourceMethod() 
+               . '?' .
+               http_build_query(array_merge($apiKey, $params));
+    }
+}
