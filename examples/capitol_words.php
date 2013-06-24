3fbdff9dad85d1b29b6a1d6bb9bd732bbb877927
lobostome <lobostome@local.dev>
2013-06-24 12:14:59 -0500

    Add Sunlight Capitol Words text resource

--- examples/capitol_words.php
+++ examples/capitol_words.php
@@ -2,13 +2,13 @@
 
 require_once 'config/config_capitol_words.php';
 
-$params = array('entity_type'   => 'legislator',
-                'entity_value'  => 'L000551');
+$params = array('phrase'    => 'health care debate',
+                'party'     => 'R');
 
 // A sample use
 try {
-    $fb->phrases->setParams($params);
-    var_dump($fb->phrases->get());
+    $fb->text->setParams($params);
+    var_dump($fb->text->get());
 } catch (\Exception $e) {
     echo $e->getMessage();
 }
