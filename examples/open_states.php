9165cff1b2cfe98a83d1d2af6ace16845406513d
lobostome <lobostome@local.dev>
2013-07-21 20:40:28 -0500

    Add Open States detailed metadata for a particular state.

--- examples/open_states.php
+++ examples/open_states.php
@@ -3,7 +3,8 @@
 require_once 'config/config_open_states.php';
 
 try {
-    var_dump($fb->metadata->get());
+    var_dump($fb->metadata_state->tx->get());
+    var_dump($fb->metadata_state->ca->get());
 } catch (\Exception $e) {
     echo $e->getMessage();
 }
