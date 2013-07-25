06d615169a3b492bcd98ce5e0d1d6a8fac33a281
lobostome <lobostome@local.dev>
2013-07-25 13:11:32 -0500

    Add Open States geo lookup resource

--- examples/open_states.php
+++ examples/open_states.php
@@ -4,7 +4,7 @@ require_once 'config/config_open_states.php';
 
 try {
     
-    var_dump($fb->legislator_detail->id('DCL000012')->get());
+    var_dump($fb->geo_lookup->coords(35.79, -78.78)->get());
     
 } catch (\Exception $e) {
     echo $e->getMessage();
