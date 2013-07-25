cd0b6333a4c2423e2259c4737b5956d43ab18eac
lobostome <lobostome@local.dev>
2013-07-25 15:29:47 -0500

    Add Open States event detail resource

--- examples/open_states.php
+++ examples/open_states.php
@@ -4,7 +4,7 @@ require_once 'config/config_open_states.php';
 
 try {
     
-    var_dump($fb->events->get(array('state' => 'tx')));
+    var_dump($fb->event_detail->id('TXE00026474')->get());
     
 } catch (\Exception $e) {
     echo $e->getMessage();
