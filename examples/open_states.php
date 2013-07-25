c2b050de284d92c1763782808396743a10c909d2
lobostome <lobostome@local.dev>
2013-07-25 15:15:20 -0500

    Add Open States committees resource

--- examples/open_states.php
+++ examples/open_states.php
@@ -4,7 +4,7 @@ require_once 'config/config_open_states.php';
 
 try {
     
-    var_dump($fb->geo_lookup->coords(35.79, -78.78)->get());
+    var_dump($fb->committees->get(array('state' => 'dc')));
     
 } catch (\Exception $e) {
     echo $e->getMessage();
