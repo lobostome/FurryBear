5b2902d405c1d30a76e7a4fd944edf57da4948cd
lobostome <lobostome@local.dev>
2013-07-25 15:26:28 -0500

    Add Open States events resource

--- examples/open_states.php
+++ examples/open_states.php
@@ -4,7 +4,7 @@ require_once 'config/config_open_states.php';
 
 try {
     
-    var_dump($fb->committee_detail->id('DCC000029')->get());
+    var_dump($fb->events->get(array('state' => 'tx')));
     
 } catch (\Exception $e) {
     echo $e->getMessage();
