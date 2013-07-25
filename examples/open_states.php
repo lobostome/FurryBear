27cd6906614c3be4a65f79b54e725858e3f21550
lobostome <lobostome@local.dev>
2013-07-25 15:18:50 -0500

    Add Open States committees detail resource

--- examples/open_states.php
+++ examples/open_states.php
@@ -4,7 +4,7 @@ require_once 'config/config_open_states.php';
 
 try {
     
-    var_dump($fb->committees->get(array('state' => 'dc')));
+    var_dump($fb->committee_detail->id('DCC000029')->get());
     
 } catch (\Exception $e) {
     echo $e->getMessage();
