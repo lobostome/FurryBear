2b55dfba46667193cb174d415de8b23bd5177a71
lobostome <lobostome@local.dev>
2013-08-12 12:02:13 -0500

    Add Congress districts locate helpers

--- src/FurryBear/Resource/SunlightCongress/Method/DistrictsLocate.php
+++ src/FurryBear/Resource/SunlightCongress/Method/DistrictsLocate.php
@@ -44,4 +44,48 @@ class DistrictsLocate extends BaseResource
         parent::__construct($furryBear);
         $this->setResourceMethod(self::ENDPOINT_METHOD);
     }
+    
+    /**
+     * Retrieves districts based on a zip
+     * 
+     * @param string $zip
+     * 
+     * @return mixed
+     */
+    public function getByZip($zip)
+    {
+        $params = array('zip' => (string)$zip);
+        return $this->get($params);
+    }
+    
+    /**
+     * Retrieves districts based on an address
+     * 
+     * @param string $address The target address
+     * 
+     * @return mixed
+     */
+    public function getByAddress($address)
+    {
+        if (is_null($this->geocodeProvider)) {
+            throw new \Exception('No geocode provider specified. To set up one, use the via() method');
+        }
+        $params = $this->geocodeProvider->geocode($address);
+        return $this->get($params);
+    }
+    
+    /**
+     * Set the geocode provider and its adapter and output strategy.
+     * 
+     * @param \FurryBear\Geocode\AbstractProvider $provider The geocode provider
+     * 
+     * @return \FurryBear\Resource\SunlightCongress\LegislatorsLocate
+     */
+    public function via(\FurryBear\Geocode\AbstractProvider $provider)
+    {
+        $this->geocodeProvider = $provider;
+        $this->geocodeProvider->setAdapter($this->furryBear->getProvider()->getAdapter());
+        
+        return $this;
+    }
 }
