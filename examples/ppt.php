8f9ef143a0d55ce979859c5b264d9293c37cafee
lobostome <lobostome@local.dev>
2013-09-19 14:11:22 -0500

    Add Political Party Time host detail resource

--- examples/ppt.php
+++ examples/ppt.php
@@ -4,7 +4,7 @@ require_once 'config/config_ppt.php';
 
 try {
     
-    var_dump($fb->hosts->get());
+    var_dump($fb->host_detail->id(1)->get());
     
 } catch (\Exception $e) {
     echo $e->getMessage();
