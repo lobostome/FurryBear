61353f9bed468c8adb3611dd0ae29e44065f035f
lobostome <lobostome@local.dev>
2013-09-19 13:51:57 -0500

    Add Political Party Time lawmaker detail resource

--- examples/ppt.php
+++ examples/ppt.php
@@ -4,7 +4,7 @@ require_once 'config/config_ppt.php';
 
 try {
     
-    var_dump($fb->lawmakers->get(array('crp_id' => 'N00003675')));
+    var_dump($fb->lawmaker_detail->id(12)->get());
     
 } catch (\Exception $e) {
     echo $e->getMessage();
